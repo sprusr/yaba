847452c59551e9106115c4ca082c92fbd502f312
abdulajet <abdulrapidz@live.com>
2018-02-25 11:36:32 -0700

    lol github innit

--- web/app.js
+++ web/app.js
@@ -1,24 +1,51 @@
-const apiKey = c66bb3467808f869596b 
-const apiSecret = d3f06effa88cd1c28de113d51569201121a133d
+import GitHub from 'github-api';
+//import Yaba from './yaba.js';
 
-const githubOAuth = require('github-oauth')({
-    githubClient: apiKey,
-    githubSecret: apiSecret,
-    baseURL: 'http://localhost',
-    loginURI: '/login',
-    callbackURI: '/callback',
-    scope: 'user' // optional, default scope is set to user 
-  })
-function login(){
-  githubOAuth.login(req, res)
+//const yaba = new Yaba();
+
+window.onload = function () {
+   //document.getElementById("loginBtn").onclick=test;
+   document.getElementById("proposeBtn").onclick=propose;
+};
+
+function propose() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var issue = document.getElementById('issue').value;
+  var bounty = document.getElementById('bounty').value;
+
+  var issueArr = issue.match(/^https?:\/\/github.com\/(.*)\/(.*)\/issues\/(.*)$/);
+
+  //1,2,3
+
+  var gh = new GitHub({
+     username: username,
+     password: password
+  });
+
+  var me = gh.getUser();
+
+  gh.getIssues(issueArr[1], issueArr[2]).createIssueComment(issueArr[3], "hi").then(issue => {
+    console.log(issue);
+  });
+
+
+
+
 }
 
-  githubOAuth.on('error', function(err) {
-    console.error('there was a login error', err)
-  })
-  
-  githubOAuth.on('token', function(token, serverResponse) {
-    console.log('here is your shiny new github oauth token', token)
-    serverResponse.end(JSON.stringify(token))
-  })
-  
+// function test() {
+//   username = document.getElementById('username').value;
+//   password = document.getElementById('password').value;
+//   githubLogin(username, password);
+// }
+//
+// function githubLogin(username, password) {
+//   // basic auth
+//   var gh = new GitHub({
+//      username: username,
+//      password: password
+//   });
+//
+//   var me = gh.getUser();
+// }
